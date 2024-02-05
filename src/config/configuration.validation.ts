aad5376a0d5445af49e052f96ac10c7847096b4b
Adept-Rohan <rohan1d20@gmail.com>
2024-02-05 13:15:17 +0545

    created a configuration module to configure the yaml file

---
+++ src/config/configuration.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const dbSchema = z.object({
+  client: z.string().default('mysql'),
+  connection: z.object({
+    host: z.string().default('localhost'),
+    port: z.number().default(3306),
+    database: z.string(),
+    user: z.string(),
+    password: z.string(),
+  }),
+});
+
+export const configSchema = z.object({
+  database: dbSchema,
+});
+
+export type Config = z.infer<typeof configSchema>;
