c66676876d1168bb6b4912ceecca3192464b36d8
Adept-Rohan <rohan1d20@gmail.com>
2024-02-05 13:16:18 +0545

    global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ConfigModule } from './config/config.module';
+import { KnexModule } from './knex/knex.module';
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-
       useFactory: () => ({
         playground: false,
         sortSchema: true,
@@ -21,6 +22,8 @@ import { join } from 'path';
         }),
       }),
     }),
+    ConfigModule,
+    KnexModule,
   ],
 })
 export class AppModule {}
